298903f41163fb697e4c1a16634737835573fafb
Kushagra Krishna <krishna619kk@gmail.com>
2026-08-12 23:15:19 +0530

    refactor: update layout configurations, eslint rules, and dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,51 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lora, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lora = Lora({
   subsets: ["latin"],
+  weight: ["400", "500"],
+  style: ["normal", "italic"],
+  variable: "--font-serif",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-sans",
 });
 
 export const metadata = {
   title: "Prept",
-  description: "Interview Ready",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang='en'
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className='min-h-full flex flex-col'>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang='en' suppressHydrationWarning>
+        <head />
+        <body className={`${lora.variable} ${dmSans.variable} font-sans`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className='min-h-screen'>{children}</main>
+            <Toaster richColors />
+
+            <footer className='relative z-10 border-t border-white/7 py-12  mx-auto px-6 flex flex-wrap items-center justify-center text-stone-400'>
+              Made with ❤️ by Kushagra Krishna
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import globals from "globals";
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
   ...nextVitals,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
